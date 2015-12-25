6cef0cdd0b46ec5126d663f7da8d0df6d9113e10
Shane <shane8641@live.com>
2015-12-25 12:11:48 -0500

    adding CrimeFragment class and extending Fragment; Uses support library

---
+++ app/src/main/java/com/example/shane/criminallintent/CrimeFragment.java
@@ -0,0 +1,10 @@
+package com.example.shane.criminallintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Shane on 12/25/2015.
+ */
+public class CrimeFragment extends Fragment {
+
+}
