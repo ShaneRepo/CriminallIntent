86ebebe73abba99842fb2290815ac67018dcc108
Shane <shane8641@live.com>
2015-12-26 14:09:06 -0500

    added new fields to Crime class, Gets and Sets for date and solved

--- app/src/main/java/com/example/shane/criminallintent/Crime.java
+++ app/src/main/java/com/example/shane/criminallintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.shane.criminallintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,10 +9,29 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 
     public Crime(){
         // Generic unique id
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
